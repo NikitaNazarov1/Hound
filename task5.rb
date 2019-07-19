76a174b3c13e4f5a1af5bc4a74bbfd453e6ecd49
mikita <nikenor11@gmail.com>
2019-07-19 12:21:47 +0300

    add solution

--- task5.rb
+++ task5.rb
@@ -1,1 +1,18 @@
+# frozen_string_literal: true
 
+MaskedString = Class.new(String)
+
+MaskedString.class_eval do
+  define_method(:tr_vowel) do
+    tr 'aeiou', '*'
+  end
+
+  define_singleton_method(:tr_vowel) do |str|
+    str.tr 'aeiou', '*'
+  end
+end
+
+s = MaskedString.new('hello')
+puts s.tr_vowel
+
+puts MaskedString.tr_vowel('goodbye')
