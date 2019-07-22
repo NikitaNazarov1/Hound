26d28f5d2f88f44dfbedc2227b4b3e096fb1087c
mikita <nikenor11@gmail.com>
2019-07-22 16:09:56 +0300

    redo solution

--- zadanie1.rb
+++ zadanie1.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
-@output = []
 x = class A
       def initialize
         @a = 11
         @@a = 22
         a = 33
         b = 43
-        a
+
+        binding
       end
       @a = 1
       @@a = 2
       a = 3
       b = 4
-      a
+
+      binding
     end
-@output << A.instance_variable_get(:@a)
-@output << A.class_variable_get(:@@a)
-@output << x
-@output << A.new.instance_variable_get(:@a)
-@output << A.class_variable_get(:@@a)
-@output << A.new.method(:initialize).call
-puts @output.sort!
+puts A.instance_variable_get(:@a)
+puts A.class_variable_get(:@@a)
+puts eval('a', x, __FILE__, __LINE__)
+puts A.new.instance_variable_get(:@a)
+puts A.class_variable_get(:@@a)
+b = A.new.method(:initialize).call
+puts eval('a', b, __FILE__, __LINE__)
