41c5c4800168306e9d3b3d1dcd1cc12294d26153
mikita <nikenor11@gmail.com>
2019-07-22 15:52:26 +0300

    rewrite binding method

--- task4.rb
+++ task4.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-the_binding = class A
-                @@a = 1
-                @a = 2
-                a = 3
+class A
+  def variables
+    @@a = 1
+    @a = 2
+    a = 3
 
-                binding
-              end
-
-p eval('[@@a, @a, a]', the_binding, __FILE__, __LINE__) # Replace '*****' to your code
+    binding
+  end
+end
+binding = A.new.variables
+p eval('[@@a, @a, a]', binding, __FILE__, __LINE__) # Replace '*****' to your code
